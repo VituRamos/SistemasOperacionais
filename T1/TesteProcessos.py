dc36b07fb7f726ee299d5eb9762973cd0303d507
Victor Ramos <107152739+VituRamos@users.noreply.github.com>
2023-10-22 13:22:11 -0300

    Update TesteProcessos.py

--- T1/TesteProcessos.py
+++ T1/TesteProcessos.py
@@ -1,5 +1,6 @@
 import csv
 
+#Ler arquivo .csv
 def ler_arquivo(nome_arquivo):
     processos = []
     tempos_cpu = []
@@ -8,48 +9,80 @@ def ler_arquivo(nome_arquivo):
     with open(nome_arquivo, 'r', newline='') as arquivo_csv:
         leitor_csv = csv.reader(arquivo_csv, delimiter=';')
         for linha in leitor_csv:
+            
+            #A partir do segundo elemento da linha "Processo" transformar em string e strip
             if linha[0] == "Processo":
-                processos = [str(elemento.strip()) for elemento in linha[1:]]
+                processos = [str(elemento.strip()) for elemento in linha[1:]] 
+                
+            #A partir do segundo elemento da linha "Tempo" transformar em int e strip
             elif linha[0] == "Tempo":
-                tempos_cpu = [int(tempo.strip()) for tempo in linha[1:]]
+                tempos_cpu = [int(tempo.strip()) for tempo in linha[1:]] 
+                
+            #A partir do segundo elemento da linha "Chegada" transformar em int e strip
             elif linha[0] == "Chegada":
-                tempos_chegada = [int(tempo.strip()) for tempo in linha[1:]]
+                tempos_chegada = [int(tempo.strip()) for tempo in linha[1:]]  
 
     return processos, tempos_cpu, tempos_chegada
 
-def sjf_preemptivo(processos, tempos_cpu, tempos_chegada):
-    tempo_atual = 0
-    linha_tempo = []
-    processos_restantes = list(range(len(processos)))
 
+#Funcao SJF 
+def sjf_preemptivo(processos, tempos_cpu, tempos_chegada):
+    
+    tempo_atual = 0 
+    linha_tempo = [] 
+    
+    #Lista indices dos processos 
+    processos_restantes = list(range(len(processos))) 
+
+    #Enquanto houverem processos restantes
     while processos_restantes:
+        
+        #Lista indices de processos com tempo de chegada <= tempo atual
         processos_disponiveis = [i for i in processos_restantes if tempos_chegada[i] <= tempo_atual]
 
+        #Se não houver tempo disponivel 
         if not processos_disponiveis:
             tempo_atual += 1
-            linha_tempo.append("_")
+            linha_tempo.append("_") #Tempo em branco
+            
+        #Se houver tempo disponivel
         else:
+            
+            #Insere processo na fila
             processos_na_fila = [(processos[i], tempos_cpu[i]) for i in processos_disponiveis]
+            
+            #Ordena o processo (Menor tempo de CPU)
             processos_na_fila.sort(key=lambda x: x[1])
+            
+            #Recebe info do processo
             processo_atual, tempo_restante = processos_na_fila[0]
 
-            for t in range(tempo_restante):
-                tempo_atual += 1
-                linha_tempo.append(processo_atual)
+            #Processo é adicionado a linha do tempo com base em seu tempo de cpu
+            tempo_atual += 1
+            linha_tempo.append(processo_atual)
+            
+            #Decrementar tempos do processo atual
+            tempos_cpu[(processos.index(processo_atual))] -=1
 
-            processo_idx = processos.index(processo_atual)
-            processos_restantes.remove(processo_idx)
+            #Remove o processo da lista de procesos restantes caso tempo de cpu zere
+            if tempos_cpu[(processos.index(processo_atual))] == 0:
+                processos_restantes.remove(processos.index(processo_atual))
 
     return linha_tempo
 
+
+#Formatar linha do tempo 
 def formatar_linha_tempo(linha_tempo):
     linha_formatada = "|0|"
     tempo = 0
     processo_atual = None
 
+    #Para cada processo em linha do tempo
     for processo in linha_tempo:
+        
         if processo == processo_atual:
             tempo += 1
+            
         else:
             if processo_atual:
                 linha_formatada += f"------{processo_atual}-----−|{tempo}|"
@@ -61,28 +94,31 @@ def formatar_linha_tempo(linha_tempo):
 
     return linha_formatada
 
-def calcular_tempos_medios(tempos_espera, tempos_resposta, num_processos):
-    tempo_medio_espera = sum(tempos_espera) / num_processos
-    tempo_medio_resposta = sum(tempos_resposta) / num_processos
+
+def calcular_tempos_medios(tempos_espera, tempos_resposta, quantidade_processos):
+    tempo_medio_espera = sum(tempos_espera) / quantidade_processos
+    tempo_medio_resposta = sum(tempos_resposta) / quantidade_processos
     return tempo_medio_espera, tempo_medio_resposta
 
-# Nome do arquivo com os dados
-nome_arquivo = "Teste.csv"  
 
-# Leitura dos dados do arquivo
+#Nome do arquivo com os dados
+nome_arquivo = "TesteP4.csv"  
+
+#Leitura dos dados do arquivo
 processos, tempos_cpu, tempos_chegada = ler_arquivo(nome_arquivo)
 
-# Executar o escalonador SJF preemptivo
+#Executar SJF preemptivo
 linha_tempo_sjf = sjf_preemptivo(processos, tempos_cpu, tempos_chegada)
 
-# Calcular tempos médios de espera e resposta
-num_processos = len(processos)
+#Calcular tempos medios
+quantidade_processos = len(processos)
 tempos_espera = [linha_tempo_sjf.index(processo) for processo in processos]
-tempos_resposta = [tempos_espera[i] - tempos_chegada[i] for i in range(num_processos)]
+tempos_resposta = [tempos_espera[i] - tempos_chegada[i] for i in range(quantidade_processos)]
+tempo_medio_espera, tempo_medio_resposta = calcular_tempos_medios(tempos_espera, tempos_resposta, quantidade_processos)
 
 linha_tempo_formatada = formatar_linha_tempo(linha_tempo_sjf)
 
-# Exibir relatório
+# Exibir resultados
 print("Processos na Fila do SJF Preemptivo:")
 print("   " + "    ".join(processos))
 print("\nTempo de CPU requerida pelos processos:")
@@ -92,6 +128,5 @@ print("   " + "    ".join(map(str, tempos_chegada)))
 print("\nLinha do Tempo:")
 print((linha_tempo_formatada))
 print("\nTempos Médios:")
-tempo_medio_espera, tempo_medio_resposta = calcular_tempos_medios(tempos_espera, tempos_resposta, num_processos)
 print(f"Tempo Médio de Espera: {tempo_medio_espera}")
 print(f"Tempo Médio de Resposta: {tempo_medio_resposta}")
